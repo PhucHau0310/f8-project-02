45b1ceab338ea821a6fe6dc174972d20eeba9be9
Phuc Hau <haunhpr024@gmail.com>
2023-06-19 20:32:24 +0700

    hoàn thành dự án 2

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ const $$ = document.querySelectorAll.bind(document);
 
 const li = $$("li");
 const liActive = $(".active");
+const feedbackItems = $$(".feedback-item");
+const dots = $$(".dot");
 
 li.forEach((li, index) => {
     li.onclick = function () {
         $(".active").classList.remove("active");
         this.classList.add("active");
     };
+});
+
+// dots.forEach((dot, index) => {
+//     dot.onclick = function () {
+//         $(".active").classList.remove("active");
+//         this.classList.add("active");
+//     };
+// });
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        // Remove the active class from all dots and feedback items
+        dots.forEach((dot) => dot.classList.remove("active"));
+        feedbackItems.forEach((item) => item.classList.remove("active"));
+
+        // Add the active class to the clicked dot and corresponding feedback item
+        dot.classList.add("active");
+        feedbackItems[index].classList.add("active");
+    });
 });
